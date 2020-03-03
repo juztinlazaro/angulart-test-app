ac247ce1e4806a37412066cfa7d73204bb7c8dfc
juztinlazaro <juztinlazaro@gmail.com>
2020-03-03 21:56:50 +0800

    .

--- src/app/store/contact/contact.initialState.ts
+++ src/app/store/contact/contact.initialState.ts
@@ -6,19 +6,19 @@ export const initialState = {
       "juztinlazaro@gmail.com",
       "Justin Lazaro",
       12314,
-      "https://lh3.googleusercontent.com/proxy/B86oAEP_bE-j_NVGUcgIzs6Db6K52p93BGuH_gL9LpCu7uRghEO6GykK6EGYDTlj-GtESWGq45sUwsakTB1Qa5kpeBcs17tEPGFu"
+      "https://careerlinkz.com/wp-content/uploads/2018/05/avatar_male.png"
     ),
     new Contacts(
       "juztinlazaro1@gmail.com",
       "Justin Lazaro",
       12314,
-      "https://lh3.googleusercontent.com/proxy/B86oAEP_bE-j_NVGUcgIzs6Db6K52p93BGuH_gL9LpCu7uRghEO6GykK6EGYDTlj-GtESWGq45sUwsakTB1Qa5kpeBcs17tEPGFu"
+      "https://careerlinkz.com/wp-content/uploads/2018/05/avatar_male.png"
     ),
     new Contacts(
       "juztinlazaro2@gmail.com",
       "Justin Lazaro",
       12314,
-      "https://lh3.googleusercontent.com/proxy/B86oAEP_bE-j_NVGUcgIzs6Db6K52p93BGuH_gL9LpCu7uRghEO6GykK6EGYDTlj-GtESWGq45sUwsakTB1Qa5kpeBcs17tEPGFu"
+      "https://careerlinkz.com/wp-content/uploads/2018/05/avatar_male.png"
     )
   ]
 };
--- src/app/views/contact-edit/contact-edit.component.ts
+++ src/app/views/contact-edit/contact-edit.component.ts
@@ -1,7 +1,29 @@
 import { Component } from "@angular/core";
+import { FormGroup, Validators, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-contact-edit",
   templateUrl: "./contact-edit.component.html"
 })
-export class ContactEditComponent {}
+export class ContactEditComponent {
+  contactForm: FormGroup;
+
+  name: string = "";
+  email: string = "";
+  phone: string = "";
+
+  ngOnInit() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      phone: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^[1-9]+[0-9]*$/)
+      ])
+    });
+  }
+
+  onSubmit() {
+    console.log("payload", this.contactForm.value);
+  }
+}
